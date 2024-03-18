507313dadd99ec7d3e7a099bfe8f82dba16e09bb
Tran Phuoc Tin <tptin.76@gmail.com>
2024-03-18 21:11:15 +0700

    second commit

--- src/main.c
+++ src/main.c
@@ -37,15 +37,6 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 	return 0;
 }
 
-
-#define TEXT_WIDTH 86
-#define TEXT_HEIGHT 16
-
-int x = (400-TEXT_WIDTH)/2;
-int y = (240-TEXT_HEIGHT)/2;
-int dx = 1;
-int dy = 2;
-
 static int update(void* userdata)
 {
 	PlaydateAPI* pd = userdata;
