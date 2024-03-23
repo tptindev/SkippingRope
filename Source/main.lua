857f5f40c82ac79b250271a0b91800d38c3b16ac
Tran Phuoc Tin <tptin.76@gmail.com>
2024-03-23 21:38:04 +0700

    update

--- Source/main.lua
+++ Source/main.lua
@@ -15,15 +15,17 @@ local COLUMN_WIDTH <const> = 0.1
 local COLUMN_HEIGHT <const> = 2.5
 local COLUMN_MASS <const> = 0
 local COLUMN_FRICTION <const> = 0.2
-local MAX_NODE <const> = 3
+local MAX_NODE <const> = 11
 local ROPE_NODE_WIDTH <const> = 0.1
 local ROPE_NODE_HEIGHT <const> = 0.1
-
+local JOINT_STEP <const> = WORLD_WIDTH / (MAX_NODE - 1)
+local MAX_MASS = 80
 local initialized = false
 local world = nil
 local floor = nil
 local rope_joints = table.create(MAX_NODE - 2, 0);
 local rope_nodes = table.create(MAX_NODE, 0)
+local columns = table.create(2, 0)
 
 function playdate.update()
     if not initialized then
@@ -59,41 +61,54 @@ function initialize()
     left_col:setCenter(COLUMN_WIDTH / 2, 2.5)
     left_col:setFriction(COLUMN_FRICTION)
     world:addBody(left_col)
-    rope_nodes[#rope_nodes + 1] = left_col
+    columns[#columns + 1] = left_col
 
+    local node_min = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 0)
+    node_min:setCenter(0,1.0)
+    node_min:setFriction(0.2)
+    world:addBody(node_min)
+    rope_nodes[#rope_nodes + 1] = node_min
+    
     for i = 1, MAX_NODE - 2, 1 do
-        local node = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 80)
-        node:setCenter(WORLD_WIDTH/2, 2)
+        print(i * 0.3 * 10)
+        local node = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 0)
+        node:setCenter(JOINT_STEP * i, 0)
         node:setFriction(0.2)
         world:addBody(node)
         rope_nodes[#rope_nodes + 1] = node
     end
 
+    local node_max = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 0)
+    node_max:setCenter(WORLD_WIDTH, 1.0)
+    node_max:setFriction(0.2)
+    world:addBody(node_max)
+    rope_nodes[#rope_nodes + 1] = node_max
+    
     local right_col = playbox.body.new(COLUMN_WIDTH, COLUMN_HEIGHT, COLUMN_MASS)
     right_col:setCenter(WORLD_WIDTH - (COLUMN_WIDTH/2), 2.5)
     right_col:setFriction(COLUMN_FRICTION)
     world:addBody(right_col)
-    rope_nodes[#rope_nodes + 1] = right_col
+    columns[#columns + 1] = right_col
 
 
     -- Create rope joints
-    for i = 1, MAX_NODE - 1, 1 do
+    for i = 1, math.floor(MAX_NODE / 2) + 1, 1 do
         local x, y = rope_nodes[i]:getCenter();
         local joint = playbox.joint.new(rope_nodes[i], rope_nodes[i + 1], x, y)
         joint:setBiasFactor(0.3)
-        joint:setSoftness(10)
+        joint:setSoftness(0.6)
         world:addJoint(joint)
         rope_joints[#rope_joints + 1] = joint
     end
 
-    -- for i = MAX_NODE, (MAX_NODE / 2) + 1, -1 do
-    --     local x, y = rope_nodes[i]:getCenter();
-    --     local joint = playbox.joint.new(rope_nodes[i], rope_nodes[i-1], x, y)
-    --     joint:setBiasFactor(0.3)
-    --     joint:setSoftness(10)
-    --     world:addJoint(joint)
-    --     rope_joints[#rope_joints + 1] = joint
-    -- end
+    for i = MAX_NODE, (MAX_NODE / 2) + 2 , -1 do
+        local x, y = rope_nodes[i]:getCenter();
+        local joint = playbox.joint.new(rope_nodes[i], rope_nodes[i - 1], x, y)
+        joint:setBiasFactor(0.3)
+        joint:setSoftness(0.6)
+        world:addJoint(joint)
+        rope_joints[#rope_joints + 1] = joint
+    end
 end
 
 function update(dt)
@@ -109,11 +124,17 @@ function draw()
     floor_polygon:close()
     graphics.fillPolygon(floor_polygon)
 
-    -- for i, node in ipairs(rope_nodes) do
-    --     local node_polygon = geometry.polygon.new(node:getPolygon())
-    --     node_polygon:close()
-    --     graphics.fillPolygon(node_polygon)
-    -- end
+    for i, col in ipairs(columns) do
+        local col_polygon = geometry.polygon.new(col:getPolygon())
+        col_polygon:close()
+        graphics.fillPolygon(col_polygon)
+    end
+
+    for i, node in ipairs(rope_nodes) do
+        local node_polygon = geometry.polygon.new(node:getPolygon())
+        node_polygon:close()
+        graphics.fillPolygon(node_polygon)
+    end
 
     -- Draw rope joints
     for i, joint in ipairs(rope_joints) do
