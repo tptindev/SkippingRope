c8ace8881ff2e4879efd68d5fcf33fa15911f435
Tran Phuoc Tin <tptin.76@gmail.com>
2024-03-23 15:25:32 +0700

    update

--- Source/main.lua
+++ Source/main.lua
@@ -15,6 +15,7 @@ local COLUMN_WIDTH <const> = 0.1
 local COLUMN_HEIGHT <const> = 2.5
 local COLUMN_MASS <const> = 0
 local COLUMN_FRICTION <const> = 0.2
+local MAX_NODE <const> = 20
 local ROPE_NODE_WIDTH <const> = 0.2
 local ROPE_NODE_HEIGHT <const> = 0.1
 
@@ -22,8 +23,8 @@ local initialized = false
 local world = nil
 local floor = nil
 local rope = nil
-local rope_joints = table.create(3,0);
-local rope_nodes = table.create(5, 0)
+local rope_joints = table.create(MAX_NODE - 2, 0);
+local rope_nodes = table.create(MAX_NODE, 0)
 local node_center = nil;
 local columns = table.create(2, 0)
 
@@ -62,60 +63,33 @@ function initialize()
     left_col:setFriction(COLUMN_FRICTION)
     world:addBody(left_col)
     columns[#columns + 1] = left_col
+    rope_nodes[#rope_nodes + 1] = left_col
+
+    for i = 1, MAX_NODE - 2, 1 do
+        local node = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 80)
+        node:setCenter(i * 0.1, 2.5)
+        node:setFriction(1.0)
+        world:addBody(node)
+        rope_nodes[#rope_nodes + 1] = node
+    end
 
     local right_col = playbox.body.new(COLUMN_WIDTH, COLUMN_HEIGHT, COLUMN_MASS)
-    right_col:setCenter(WORLD_WIDTH - COLUMN_WIDTH / 2, 2.5)
+    right_col:setCenter(WORLD_WIDTH - (COLUMN_WIDTH/2), 2.5)
     right_col:setFriction(COLUMN_FRICTION)
     world:addBody(right_col)
     columns[#columns + 1] = right_col
+    rope_nodes[#rope_nodes + 1] = right_col
 
-    local node_1 = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 80)
-    node_1:setCenter(1, 0.1)
-    node_1:setFriction(0.1)
-    world:addBody(node_1)
-    rope_nodes[#rope_nodes + 1] = node_1
-
-    node_center = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 80)
-    node_center:setCenter(2.5, 0.1)
-    node_center:setFriction(0.1)
-    world:addBody(node_center)
-    rope_nodes[#rope_nodes + 1] = node_center
-    
-    local node_2 = playbox.body.new(ROPE_NODE_WIDTH, ROPE_NODE_HEIGHT, 80)
-    node_2:setCenter(4, 0.1)
-    node_2:setFriction(0.1)
-    world:addBody(node_2)
-    rope_nodes[#rope_nodes + 1] = node_2
 
     -- Create rope joints
-    local rope_joint_0 = playbox.joint.new(left_col, node_1, 0, 1.5)
-    rope_joint_0:setBiasFactor(0.2)
-    rope_joint_0:setSoftness(1)
-    world:addJoint(rope_joint_0)
-    rope_joints[#rope_joints + 1] = rope_joint_0
-
-    local rope_joint_3 = playbox.joint.new(right_col, node_2, WORLD_WIDTH, 1.5)
-    rope_joint_3:setBiasFactor(0.2)
-    rope_joint_3:setSoftness(1)
-    world:addJoint(rope_joint_3)
-    rope_joints[#rope_joints + 1] = rope_joint_3
-
-    local x1, y1 = node_1:getCenter()
-    local rope_joint_1 = playbox.joint.new(node_1, node_center, x1, y1)
-    rope_joint_1:setBiasFactor(0.3)
-    rope_joint_1:setSoftness(1)
-    world:addJoint(rope_joint_1)
-    rope_joints[#rope_joints + 1] = rope_joint_1
-
-    local x2, y2 = node_2:getCenter()
-    local rope_joint_2 = playbox.joint.new(node_2, node_center, x2, y2)
-    rope_joint_2:setBiasFactor(0.3)
-    rope_joint_2:setSoftness(1)
-    world:addJoint(rope_joint_2)
-    rope_joints[#rope_joints + 1] = rope_joint_2
-
-
-
+    for i = 1, MAX_NODE - 1, 1 do
+        local x, y = rope_nodes[i]:getCenter();
+        local joint = playbox.joint.new(rope_nodes[i], rope_nodes[i+1], x, y)
+        joint:setBiasFactor(1)
+        joint:setSoftness(1)
+        world:addJoint(joint)
+        rope_joints[#rope_joints + 1] = joint
+    end
 end
 
 function update(dt)
