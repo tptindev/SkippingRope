e1e6d85201f8df8fea5276c5c8d32b6403e9215e
Tran Phuoc Tin <tptin.76@gmail.com>
2024-06-23 01:09:18 +0700

    update

--- src/ecs/system.c
+++ src/ecs/system.c
@@ -4,8 +4,11 @@
 void UpdatePosition(PlaydateAPI* api, Position* position, float dt)
 {
 	double angle_rad = api->system->getCrankAngle() * (3.14159265358979323846f / 180.0f);
-	position->x = (float)(0.5f * cos(angle_rad));
-	position->y = (float)(0.5f * sin(angle_rad));
+	if (angle_rad > 0)
+	{
+		position->x = (float)(0.5f * cos(angle_rad));
+		position->y = (float)(0.5f * sin(angle_rad));
+	}
 }
 
 void UpdateHealth(Health* health, float dt)
